005ec5698e00fc8b0b38a67a22b25dd3c8351ac6
hikio-17 <fajri010217@gmail.com>
2023-07-30 23:49:56 +0700

    add model Todo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { json } from 'body-parser';
 import { routerTodo } from './routes/todo';
 
@@ -6,6 +7,9 @@ const app = express();
 
 app.use(json());
 app.use(routerTodo);
+
+/** CONNET TO DATABASE */
+mongoose.connect('mongodb://localhost:27017/todo')
 
 const PORT = 3000;
 
